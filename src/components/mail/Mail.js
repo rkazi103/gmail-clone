73172343ac31d56371796073320b6442aaf5d76b
Rayan Kazi <rayan.quack.you@gmail.com>
2021-06-24 12:39:45 -0700

    built the mail screen

--- src/components/mail/Mail.js
+++ src/components/mail/Mail.js
@@ -1,8 +1,101 @@
+import {
+  Container,
+  Tools,
+  ToolsLeft,
+  ToolsRight,
+  Body,
+  BodyHeader,
+  BodyImportant,
+  Message,
+  Time,
+} from "./MailStyle";
+import { IconButton } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import PrintIcon from "@material-ui/icons/Print";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
 const Mail = () => {
+  const history = useHistory();
+
   return (
-    <div>
-      <h1>Mail</h1>
-    </div>
+    <Container>
+      <Tools>
+        <ToolsLeft>
+          <IconButton onClick={() => history.push("/")}>
+            <ArrowBackIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </ToolsLeft>
+
+        <ToolsRight>
+          <IconButton>
+            <UnfoldMoreIcon />
+          </IconButton>
+
+          <IconButton>
+            <PrintIcon />
+          </IconButton>
+
+          <IconButton>
+            <ExitToAppIcon />
+          </IconButton>
+        </ToolsRight>
+      </Tools>
+
+      <Body>
+        <BodyHeader>
+          <h2>Subject</h2>
+          <BodyImportant />
+          <p>Title</p>
+          <Time>10pm</Time>
+        </BodyHeader>
+
+        <Message>
+          <p>This is a message</p>
+        </Message>
+      </Body>
+    </Container>
   );
 };
 
--- src/components/mail/MailStyle.js
+++ src/components/mail/MailStyle.js
@@ -1,3 +1,66 @@
 import styled from "styled-components";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  flex: 1;
+  background-color: whitesmoke;
+`;
+
+export const Tools = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+`;
+
+export const ToolsLeft = styled.div`
+  display: flex;
+`;
+
+export const ToolsRight = styled.div``;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 30px;
+  background-color: white;
+  padding: 20px;
+  height: 100vh;
+  box-shadow: 0px 5px 7px 0px rgba(0, 0, 0, 0.24);
+`;
+
+export const BodyHeader = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+  padding: 20px;
+  position: relative;
+
+  > h2 {
+    font-weight: 400;
+    margin-right: 20px;
+  }
+
+  > p {
+    margin-left: 20px;
+  }
+`;
+
+export const BodyImportant = styled(LabelImportantIcon)`
+  color: #e8ab02 !important;
+`;
+
+export const Time = styled.p`
+  position: absolute;
+  top: 24px;
+  right: 0;
+  font-size: 12px;
+  color: gray;
+`;
+
+export const Message = styled.div`
+  > p {
+    padding: 10px;
+    margin-right: 20px;
+    overflow-wrap: break-word;
+  }
+`;
